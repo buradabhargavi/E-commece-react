c24b05a70f02dfdae8e3bc9f63da486a159cd770
buradabhargavi <burada.bhargavi@actualpixel.in>
2024-09-04 10:40:55 +0530

    Aboutus

---
+++ src/Assets/Images.js
@@ -0,0 +1,1 @@
+export const aboutImage = require("./ecommerce.png");
--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -2,12 +2,13 @@ import React from "react";
 import NavBar from "../../GlobalComponents/NavBar";
 import Header from "../../GlobalComponents/Header";
 import Footer from "../../GlobalComponents/Footer";
-
+import AboutusBody from "../../Components/AboutUs/AboutusBody";
 function AboutUs() {
   return (
     <>
       <NavBar></NavBar>
       <Header></Header>
+      <AboutusBody />
       <Footer></Footer>
     </>
   );
